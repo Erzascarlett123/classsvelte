5cc4d38214af5b461ab9ddfbbe3d067145e57a14
erza <satriaseptian724@gmail.com>
2024-10-19 14:40:31 +0700

    terserah

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,39 +1,36 @@
 <script lang="ts">
-	import { goto } from '$app/navigation'; // Tambahkan `goto` untuk redirect
+	import { invalidate } from '$app/navigation';
 	import { onMount } from 'svelte';
 	import { page } from '$app/stores';
 	import NavbarGuru from '$lib/component/guru.svelte';
 	import Navbar from '$lib/component/navbar.svelte';
 	import "../app.css";
-	
+  
 	// Logika navbar seperti yang sudah ada
 	let showNavbar = true;
 	export let data;
-	let session;
-	let supabase;
-  
-	// Pengecekan autentikasi pengguna
 	$: ({ session, supabase } = data);
-	
-	onMount(async () => {
-	  // Cek session pengguna
-	  const { data: { session } } = await supabase.auth.getSession();
+  
+	onMount(() => {
+	  const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+		if (newSession?.expires_at !== session?.expires_at) {
+		  invalidate('supabase:auth');
+		}
+	  });
+  
+	  return () => data.subscription.unsubscribe();
 	});
-	
-	// Menentukan apakah navbar harus disembunyikan di beberapa path
+  
 	$: {
-	  const hideNavbarOnPaths = ['/private/dashboard guru','/private/dashboard guru/Send', '/auth', '/auth/error', '/', '/private/dashboard murid','/private'];
+	  const hideNavbarOnPaths = ['/private/dashboard guru','/private/dashboard guru/Send', '/auth','/auth/error', '/', '/private/dashboard murid','/private'];
 	  showNavbar = !hideNavbarOnPaths.includes($page.url.pathname);
 	}
   </script>
-  
-  <!-- Jika navbar diizinkan untuk muncul -->
-  {#if showNavbar}
-	{#if $page.url.pathname.includes('guru')}
-	  <NavbarGuru />
-	{/if}
-	<Navbar />
+
+{#if showNavbar}
+  {#if $page.url.pathname.includes('guru')}
+    <NavbarGuru />
   {/if}
-  
-  <slot />
-  
+  <Navbar />
+{/if}
+
