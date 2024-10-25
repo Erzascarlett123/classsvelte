bd5b1766a350e76d0f490eeb8aa5be0e0b58064e
erza <satriaseptian724@gmail.com>
2024-10-25 14:11:11 +0700

    wanjeng

--- src/routes/auth/+layout.svelte
+++ src/routes/auth/+layout.svelte
@@ -1,21 +1,6 @@
-<script lang="ts">
-    import { navigate } from 'svelte-routing'; // atau metode navigasi lainnya
-
-const guruEmails = ['satriaseptian724@gmail.com', 'guru2@gmail.com']; // Ganti dengan email guru yang diizinkan
-let email = '';
-
-function handleLogin() {
-    if (guruEmails.includes(email)) {
-        navigate('/private/dashboard%20guru'); // Redirect ke halaman dashboard guru
-    } else {
-        navigate('/private/dashboard%20murid'); // Redirect ke halaman dashboard murid
-    }
-}
-</script>
-
 <header>
-  <nav>
-    <a href="/"></a>
-  </nav>
+	<nav>
+		<a href="/"></a>
+	</nav>
 </header>
 <slot />
